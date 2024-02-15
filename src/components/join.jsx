83f31e1ffb4295778f086c9d8268c080adfe0c26
Md Asaduzaman <asadasad9282@gmail.com>
2024-02-16 01:04:03 +0600

    changes

--- src/components/join.jsx
+++ src/components/join.jsx
@@ -1,3 +1,4 @@
+import img from '../assets/images/pic-01.jpg';
 
 const Join = () => {
   return (
@@ -5,7 +6,19 @@ const Join = () => {
 
       <h2 className=' bg-gradient-to-r from-[#7BEFFF] to-[#07C926] inline-block  bg-clip-text text-transparent text-2xl font-semibold' >why you</h2>
 
-      <div>
+      <div className="body">
+        <div className="box">
+          <div className='content'>
+
+            <img className='' src={img} alt="" />
+            <div className='flex flex-col'>
+
+              <h2>Lorem, ipsum dolor.</h2>
+              <p className='w-[100px] cursor-pointer text-center mx-auto'>Connect</p>
+            </div>
+          </div>
+        </div>
+
 
       </div>
     </div>
@@ -13,3 +26,4 @@ const Join = () => {
 };
 
 export default Join;
+// absolute top-0 left-0 w-full h-full object-cover p-[7px] z-30 rounded-full
