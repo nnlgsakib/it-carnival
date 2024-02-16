9a7a862faea90f98da08da262dfef9c168c59cff
Md Asaduzaman <asadasad9282@gmail.com>
2024-02-16 10:40:14 +0600

    changes

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -21,21 +21,23 @@ const Banner = () => {
           }}
         >
           <div className=" bg-black h-full  bg-opacity-50">
-            <div className="text-[#fff] absolute top-1/4 lg:top-1/4 px-5">
-              <h1 className="text-xl md:text-3xl lg:text-5xl font-semibold text-white ">
-                {"The nine'th anniversary of Edulife IT Institute"}
-              </h1>
-              <p className=" font-medium mt-2">
-                Hilltracts IT Cornival Lorem ipsum dolor sit amet consectetur
-                adipisicing elit.{" "}
-                <span className="text-[#21c964]">Edulife It instute</span>
-              </p>
-              <div>
-                <div id=" ">
+            <div className="max-w-[1280px] mx-auto">
+              <div className="text-[#fff] absolute top-1/4 lg:top-1/2 px-5">
+                <h1 className="text-xl md:text-3xl lg:text-5xl font-semibold text-white ">
+                  {"The nine'th anniversary of Edulife IT Institute"}
+                </h1>
+                <p className=" font-medium mt-2">
+                  Hilltracts IT Cornival Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit.{" "}
+                  <span className="text-[#21c964]">Edulife It instute</span>
+                </p>
+                <div>
+                  <div id=" ">
 
-                  <button onClick={() => scrollToTextSection()} className="bg-gradient-to-l from-[#19c3fc] to-slate-800 text-white mt-5 lg:mt-8 px-3 lg:px-8 py-1 lg:py-2 rounded-md  hover:scale-[1.03] duration-200 border-2  font-medium lg:text-lg">
-                    Register Now
-                  </button>
+                    <button onClick={() => scrollToTextSection()} className="bg-gradient-to-l from-[#19c3fc] to-slate-800 text-white mt-5 lg:mt-8 px-3 lg:px-8 py-1 lg:py-2 rounded-md  hover:scale-[1.03] duration-200 border-2  font-medium lg:text-lg">
+                      Register Now
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
