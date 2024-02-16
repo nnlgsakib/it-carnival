ce863aeaa30083e23fb4b818555a596cfd4c9af4
Remon Tripura <remontripura045@gmail.com>
2024-02-16 12:34:32 +0600

    content update

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -16,9 +16,8 @@ const About = () => {
             <div className="w-8 h-1 bg-gradient-to-r from-cyan-400  to-[#893eba] bg-white rounded-sm"></div>
           </div>
           <div className=" mt-5 text-gray-300 text-lg">
-            <p className="mt-4 "> <strong className="text-cyan-400">HILL TRACTS IT CARNIVAL</strong> is a community conference for developers, admins, DevOps engineers, testers, documentation writers and other contributors to open source technologies.</p>
-            <p className="mt-4 ">What’s next? Any topics that are discussed or actively researched yet their impact is still potential. Best practices and great examples of open source research collaborations are welcome. Trending topics from software development, metaverse, new trends in existing areas</p>
-            <p className="mt-4 ">Artificial intelligence, large language models, data storing and processing, best practices for data lakes/data mesh, and parallelism and vector computing.</p>
+            <p className="mt-4 ">Hill Tracts IT Carnival 2024 is a pioneering event aimed at fostering innovation, collaboration, and empowerment within the IT community of the Chittagong Hill Tracts region. This landmark gathering serves as a platform for IT entrepreneurs, freelancers, and online professionals to come together, share knowledge, and explore opportunities for growth and development.</p>
+            <p className="mt-4 ">Our mission is to catalyze the growth and advancement of the IT ecosystem in the Chittagong Hill Tracts region by providing a dynamic platform for networking, learning, and collaboration. Our vision is to establish the Hill Tracts IT Carnival as the premier event for showcasing innovation, driving entrepreneurship, and empowering individuals to harness the transformative power of technology for positive change.</p>
           </div>
         </div>
       </div>
--- src/components/speakers.jsx
+++ src/components/speakers.jsx
@@ -1,63 +1,82 @@
-import speakers5 from '../assets/images/pic-01.jpg';
-import speakers2 from '../assets/images/pic-02.jpg';
-import speakers1 from '../assets/images/pic-03.jpg';
-import speakers3 from '../assets/images/pic-04.jpg';
-
+import speakers5 from "../assets/images/pic-01.jpg";
+import speakers2 from "../assets/images/pic-02.jpg";
+import speakers1 from "../assets/images/pic-03.jpg";
+import speakers3 from "../assets/images/pic-04.jpg";
 
 const Speakers = () => {
   return (
     <div className=" mt-5 lg:mt-8">
-      <div className='my-5'>
-        <div className='pb-3'>
-          <h2 className="text-2xl lg:pb-4 lg:text-4xl  text-white font-semibold  ">Speakers</h2>
+      <div className="my-5">
+        <div className="pb-3">
+          <h2 className="text-2xl lg:pb-4 lg:text-4xl  text-white font-semibold  ">
+            Speakers
+          </h2>
           <div className="w-8 h-1 mt-1 bg-gradient-to-r from-cyan-400  to-[#893eba] bg-white rounded-sm"></div>
-
         </div>
 
-        <h2 className='font-medium'>Join us at  <span className="text-gradient-to-r from-[#071448] to-[#1f068c]">HillTracts it cernival</span> to learn from the best and brightest minds in programming,</h2>
-
+        <h2 className="font-medium">
+          Join us at{" "}
+          <span className="text-gradient-to-r from-[#071448] to-[#1f068c]">
+            HillTracts it cernival
+          </span>{" "}
+          to learn from the best and brightest minds in programming,
+        </h2>
       </div>
 
+      <div className="mx-auto grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center gap-5 lg:gap-8">
+        <div className="w-[250px] text-center">
+          <div className="  speakersIamgeContainer borderColor">
+            <img className="speakersIamge" src={speakers5} alt="" />
+          </div>
+          <div className="">
+            <h2 className="speakersName">Hasin Hayder</h2>
+            <p className="text-[#d8d8d8] font-medium">
+              IT industry expert and extraordinary visionary in web development
+            </p>
+          </div>
+        </div>
+        <div className="w-[250px] text-center">
+          <div className="speakersIamgeContainer borderColor">
+            <img className="speakersIamge" src={speakers2} alt="" />
+          </div>
+          <div className="">
+            <h2 className="speakersName ">Rahitul Islam</h2>
+            <p className="text-[#d8d8d8] font-medium">
+              popular information technology writer and journalist
+            </p>
+          </div>
+        </div>
+        <div className="w-[250px] text-center">
+          <div className="speakersIamgeContainer borderColor">
+            <img
+              className="rounded-full w-full h-full hover:scale-105 duration-300"
+              src={speakers3}
+              alt=""
+            />
+          </div>
+          <div className="">
+            <h2 className="speakersName ">Rabbil Hasan</h2>
+            <p className="text-[#d8d8d8] font-medium">
+              senior software engineer, IDLC
+            </p>
+          </div>
+        </div>
 
-      <div className='mx-auto grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center gap-5 lg:gap-8'>
-        <div className='w-[250px] text-center'>
-          <div className='  speakersIamgeContainer borderColor'>
-            <img className='speakersIamge' src={speakers5} alt="" />
+        <div className="w-[250px] text-center">
+          <div className="speakersIamgeContainer borderColor">
+            <img
+              className="rounded-full hover:scale-105 duration-300"
+              src={speakers1}
+              alt=""
+            />
           </div>
-          <div className=''>
-            <h2 className='speakersName'>Rabbil hasan</h2>
-            <p className='text-[#d8d8d8] font-medium'>Founder & Educator, Learn with Rabbil Hasan</p>
-          </div>
-        </div>
-        <div className='w-[250px] text-center'>
-          <div className='speakersIamgeContainer borderColor'>
-            <img className='speakersIamge' src={speakers2} alt="" />
-          </div>
-          <div className=''>
-            <h2 className='speakersName '>Rabbil hasan</h2>
-            <p className='text-[#d8d8d8] font-medium'>Founder & Educator, Learn with Rabbil Hasan</p>
+          <div className="">
+            <h2 className="speakersName ">Masum Billah Bhuiyan</h2>
+            <p className="text-[#d8d8d8] font-medium">
+              founder of Giant Marketers
+            </p>
           </div>
         </div>
-        <div className='w-[250px] text-center'>
-          <div className='speakersIamgeContainer borderColor'>
-            <img className='rounded-full w-full h-full hover:scale-105 duration-300' src={speakers3} alt="" />
-          </div>
-          <div className=''>
-            <h2 className='speakersName '>Rabbil hasan</h2>
-            <p className='text-[#d8d8d8] font-medium'>Founder & Educator, Learn with Rabbil Hasan</p>
-          </div>
-        </div>
-
-        <div className='w-[250px] text-center'>
-          <div className='speakersIamgeContainer borderColor'>
-            <img className='rounded-full hover:scale-105 duration-300' src={speakers1} alt="" />
-          </div>
-          <div className=''>
-            <h2 className='speakersName '>Rabbil hasan</h2>
-            <p className='text-[#d8d8d8] font-medium'>Founder & Educator, Learn with Rabbil Hasan</p>
-          </div>
-        </div>
-
       </div>
     </div>
   );
