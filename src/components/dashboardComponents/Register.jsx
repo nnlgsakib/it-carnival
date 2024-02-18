fe74d434bf40693719dadc8cf139683feb633073
Md Asaduzaman <asadasad9282@gmail.com>
2024-02-18 13:20:30 +0600

    register from

--- src/components/dashboardComponents/Register.jsx
+++ src/components/dashboardComponents/Register.jsx
@@ -1,13 +1,49 @@
 import { useForm } from "react-hook-form";
-import { NavLink } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Register = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const { register, handleSubmit, formState: { errors }, } = useForm();
+  const onSubmit = (data) => {
+
+    console.log(data);
+
+    fetch("http://localhost:5000/api/v1/admin/create", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        // Add any additional headers if needed
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((responseData) => {
+        console.log(responseData);
+        // console.log(responseData?.errorMessages[0]?.message);
+        if (responseData?.success == false) {
+          return Swal.fire({
+            icon: "error",
+            title: "Faield",
+            text: `${responseData?.errorMessages[0]?.message}`,
+          });
+        }
+        if (responseData?.success == true) {
+          return Swal.fire({
+            icon: "success",
+            title: "Register success",
+            text: `${responseData?.message}`,
+          });
+        }
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error('Error fetching data:', error);
+      });
+
+  };
   return (
     <div className=" py-5 pb-10 w-full">
-      <h2 className="text-3xl text-center py-5 font-semibold ">Register</h2>
-      <div className="bg-gray-900 rounded-lg w-6/12 mx-auto p-8">
+      <h2 className="text-3xl text-center py-5 font-semibold ">Create Admin</h2>
+      <div className="bg-gray-900 rounded-lg lg:w-6/12 lg:mx-auto mx-2 p-8">
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="flex flex-col gap-5">
             <div>
@@ -19,8 +55,11 @@ const Register = () => {
                 type="text"
                 id="name"
                 {...register("name", { required: true })}
-                placeholder="Type your name"
+                placeholder="Name"
               />
+              {errors.name && (
+                <span className="text-red-500 text-xs">Name is required</span>
+              )}
             </div>
             <div>
               <label
@@ -31,45 +70,35 @@ const Register = () => {
               </label>
               <input
                 className="w-full px-4 py-2 rounded-lg border border-red-400 outline-blue-300 text-[#000]"
-                type="number"
+                type="string"
                 id="number"
-                {...register("number", { required: true })}
-                placeholder="Your phone number"
+                {...register("phone", { required: true })}
+                placeholder="Phone"
               />
+              {errors.number && (
+                <span className="text-red-500 text-xs">Phone is required</span>
+              )}
             </div>
             <div>
-              <label className="block mb-2 text-[#FFFFFF]" htmlFor="email">
-                Email
-              </label>
-              <input
-                className="w-full px-4 py-2 rounded-lg border border-red-400 outline-blue-300 text-[#000]"
-                type="email"
-                id="email"
-                {...register("email", { required: true })}
-                placeholder="email"
-              />
-            </div>
-            <div>
-              <label className="block mb-2 text-[#FFFFFF]" htmlFor="trxid">
-                Trx Id
-              </label>
-              <input
-                className="w-full px-4 py-2 rounded-lg border border-red-400 outline-blue-300 text-[#000]"
-                type="text"
-                id="trxid"
-                {...register("trxid", { required: true })}
-                placeholder="Trx Id"
-              />
-            </div>
-          
-            <div>
-              <NavLink
-                to="/dashboard/login"
-                className="text-blue-500 hover:text-blue-900 duration-150"
+              <label
+                className="block mb-2 text-[#FFFFFF]"
+                htmlFor="password"
               >
-                Alredy have an account?
-              </NavLink>
+                Password
+              </label>
+
+              <input
+                className="w-full px-4 py-2 rounded-lg border border-red-400 outline-blue-300 text-[#000]"
+                type="string"
+                id="password"
+                {...register("password", { required: true })}
+                placeholder="Password"
+              />
+              {errors.password && (
+                <span className="text-red-500 text-xs">Password is required</span>
+              )}
             </div>
+
           </div>
           <button
             className="w-full bg-gradient-to-l from-[#19c3fc] to-slate-800 rounded-lg py-2 font-semibold mt-10"
