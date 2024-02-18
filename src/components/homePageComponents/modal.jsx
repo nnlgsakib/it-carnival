7e3cbdd192ffc39b09f2697dd330bc26ccbeb790
Md Asaduzaman <asadasad9282@gmail.com>
2024-02-18 15:32:21 +0600

    change image

--- src/components/homePageComponents/modal.jsx
+++ src/components/homePageComponents/modal.jsx
@@ -1,6 +1,7 @@
 import html2canvas from 'html2canvas';
 import { useRef } from 'react';
-import image from '../../assets/about.png';
+// import image from '../../assets/about.png';
+import image from '../../assets/registrationTiket.jpg';
 
 import PropTypes from 'prop-types';
 
@@ -10,12 +11,12 @@ const Modal = ({ isOpen, onClose }) => {
   const captureWebPage = async () => {
     try {
       const canvas = await html2canvas(containerRef.current);
-      const image = canvas.toDataURL("image/jpeg");
+      const image = canvas.toDataURL("image/jpg");
 
       // Create a link element to trigger the download
       const link = document.createElement("a");
       link.href = image;
-      link.download = "webpage.jpg";
+      link.download = "titket.jpg";
       document.body.appendChild(link);
 
       // Trigger the download
@@ -27,6 +28,8 @@ const Modal = ({ isOpen, onClose }) => {
       console.error("Error capturing web page:", error);
     }
   };
+
+
   return (
     <div className=''>
       <>
@@ -39,39 +42,32 @@ const Modal = ({ isOpen, onClose }) => {
               <div className=''>
                 <div
                   onClick={onClose}
-                  className='text-end cursor-pointer p-3 bg-gray-900 w-10 h-10 ms-auto flex  justify-center place-items-center rounded-full'
+                  className='text-end cursor-pointer p-3 mb-2 bg-gray-900 w-10 h-10 ms-auto flex  justify-center place-items-center rounded-full'
                 >
                   X
                 </div>
-                <div className='max-w-screen  w-[300px] font-semibold     '>
-                  <div>
-                    <p className=' text-md mb-[30px] text-center lg:text-2xl'>
-                      Select asset to send
-                    </p>
-                  </div>
+                <div className='max-w-screen  w-[400px]     font-semibold     '>
+
                   <div>
                     <div className=''>
 
                       <div ref={containerRef} >
                         {/* Include the HTML content of the webpage you want to capture */}
-                        <div className='relative'>
-                          <img className='w-[300px]' src={image} alt='' />
-                          <div>
-
-                            <h1 className='absolute top-0 mx-1'>Your Web Page Content </h1>
-                            <div className='absolute bottom-12'>
-                              <h1 className='absolute bottom-12 mx-1'>NO: C-00001                           <p>01568242812</p>
-                              </h1>
-                              <h1 className='absolute bottom-7 mx-1'>Asaduzzaman1156@gmail.com</h1>
-                            </div>
+                        <div className='relative w-[400px] '>
+                          <img className='w-[500px]' src={image} alt='' />
+                          <div className='absolute left-[138px] bottom-44 text-[#8c40bf]'>
+                            <h1 className='text-end text-[#3a0f57]'>C-00001</h1>
+                            <p>Md. Asaduzzaman Asad</p>
+                            <h1 className=''>Asaduzzaman1156@gmail.com</h1>
                           </div>
-                          <p>Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet.</p>
                         </div>
                       </div>
-                      <button onClick={captureWebPage} className="bg-gradient-to-r from-[#19c3fc] to-[#19d349] text-white mt-5 lg:mt-8  px-8 py-2 rounded-md  hover:scale-[1.03] duration-200 font-medium text-md">
-                        Download
-                      </button>
-                      <button className='' onClick={captureWebPage}></button>
+                      <div className='flex justify-end mt-4'>
+                        <button onClick={captureWebPage} className="bg-gradient-to-r from-[#19c3fc] to-[#19d349] text-white mt-5 lg:mt-8  px-8 py-2 rounded-md hover:scale-[1.03] duration-200 font-medium">
+                          Download
+                        </button>
+                        <button className='' onClick={captureWebPage}></button>
+                      </div>
                     </div>
                   </div>
                 </div>
